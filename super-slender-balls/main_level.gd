76f195fc5c1ec5d2314057707a6a26736a411a5e
crayonmelon <ciaranmalone27@gmail.com>
2025-10-31 19:06:56 +0000

    changes

---
+++ super-slender-balls/main_level.gd
@@ -0,0 +1,25 @@
+extends Node3D
+@onready var area_3d: Area3D = $spen/Area3D
+
+
+@export var level_1: PackedScene
+
+var mouse_in = false
+
+func _ready() -> void:
+	area_3d.mouse_entered.connect(mouse_entered)
+	area_3d.mouse_exited.connect(mouse_exit)
+	
+
+
+func _input(event: InputEvent)-> void:
+	if event.is_action_pressed("START") and mouse_in:
+		get_tree().change_scene_to_packed(level_1)
+
+
+func mouse_entered() -> void:
+	mouse_in = true
+
+
+func mouse_exit() -> void:
+	mouse_in = false
